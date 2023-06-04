cd1620d7fcbf64f6df83d3d35eaa47a09b5763cd
BobosilaVictor <vbobosila1@gmail.com>
2023-06-05 01:13:19 +0300

    This is wrong, so wrong, but im tired. Upgrade python version to 3.10.4 for sanity

--- backend/start_websockets.sh
+++ backend/start_websockets.sh
@@ -1,9 +1,9 @@
 export PYTHONPATH=$PWD
 
 
-python3 backend/websockets/websocket_device_finder.py &
-python3 backend/websockets/websocket_device_group.py &
-python3 backend/websockets/websocket_device_setter.py &
-python3 backend/websockets/websocket_redis_query.py &
-python3 backend/websockets/websocket_response_bind.py 
+python3.10 backend/websockets/websocket_device_finder.py &
+python3.10 backend/websockets/websocket_device_group.py &
+python3.10 backend/websockets/websocket_device_setter.py &
+python3.10 backend/websockets/websocket_redis_query.py &
+python3.10 backend/websockets/websocket_response_bind.py 
     
